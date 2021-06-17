da8fdb5a8c63e078b0e84481369f65ef28828c64
alex-jin45 <zhengzhengjin999@gmail.com>
2021-06-17 11:44:27 +0200

    add Page React-Spring

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,9 @@ const Home = () => {
   return (
     <>
       <h1>Hello World</h1>
+      <nav>
+        <a href=''>React-Spring</a>
+      </nav>
     </>
   );
 };
---
+++ src/pages/ReactSpring.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ReactSpring = () => {
+  return (
+    <>
+      <h1>ReactSpring</h1>
+    </>
+  );
+};
+
+export default ReactSpring;
